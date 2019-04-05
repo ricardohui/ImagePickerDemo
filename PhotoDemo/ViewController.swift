753002a318bb26d8b31bd35e43b03b381e4334f9
Ricardo Hui <eehaha@gmail.com>
2019-04-06 07:20:55 +0800

    Image Picker and display image

--- PhotoDemo/ViewController.swift
+++ PhotoDemo/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = image
+        }else{
+            print("There was a problem getting the image")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        
+        imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        self.present(imagePickerController, animated: true, completion: nil)
+        
+        
+    }
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
